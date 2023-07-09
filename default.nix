5fd8b772ab992af02e5518b952746633814d116a
Kenichi Kamiya <kachick1@gmail.com>
2023-07-09 20:23:53 +0900

    Initial commit

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz") { } }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nil
+    pkgs.nixpkgs-fmt
+    pkgs.dprint
+  ];
+}
