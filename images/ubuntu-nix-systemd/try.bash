50bceae9d03129913bf64dbf62d82a4f71954d6c
Kenichi Kamiya <kachick1@gmail.com>
2024-05-20 16:08:02 +0900

    Replace dprint prettier plugin with yamlfmt

--- images/ubuntu-nix-systemd/try.bash
+++ images/ubuntu-nix-systemd/try.bash
@@ -5,7 +5,7 @@ set -euxo pipefail
 image_tag=ubuntu-nix-systemd
 user="$1"
 
-podman build --tag "$image_tag" --build-arg username="$user" --file "./images/${image_tag}/Containerfile"  .
+podman build --tag "$image_tag" --build-arg username="$user" --file "./images/${image_tag}/Containerfile" .
 container_id="$(podman run --rm --detach "$image_tag")"
 sleep 1 # Wait systemd to be ready
 podman exec --user "$user" --interactive --tty "$container_id" bash
